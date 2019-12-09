0f398fc5ca8df34432bca35d5ee5cdd10ad5d276
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-09 16:41:26 +0530

    ADDED: Store Singlet H or T in Dict

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -2,11 +2,26 @@
 
 echo "Welcom to FlipCoinSimulator"
 
-flip=$((RANDOM%2))
+head=0
+tail=0
+no=20
+declare -A flipCoin
 
-if(( flip == 1 ))
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+for (( i=0;$i<$no;i++ ))
+do
+	flip=$((RANDOM%2))
+
+	if(( flip == 1 ))
+	then
+	head=$(($head+1))
+	flipCoin[$head]="H"
+	else
+	tail=$(($tail+1))
+	flipCoin[$tail]="T"
+	fi
+done
+
+headPer=$(( ($head*100)/$no ))
+tailPer=$(( ($tail*100)/$no ))
+
+echo ${flipCoin[@]}
