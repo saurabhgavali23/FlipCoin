b229b0ad0c135cdd8413b1953cce3d0a6c859117
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-13 15:39:22 +0530

    MODIFIED: Variable Name, number to numOfCoin

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -2,68 +2,65 @@
 
 echo "Welcom to FlipCoinSimulator"
 
-number=0
-
-function flipCoin(){
-
-num=$1
-number=$2
-key=""
+numOfCoins=0
 
 declare -A combinations
 
-for (( i=0;i<$number;i++ ))
-do
+function getFlipCoinResult(){
 
-	for ((j=0;j<$num;j++))
-	do
-		flip=$((RANDOM%2))
-
-	if (( flip == 1 ))
-	then
-		key=$key"H"
-	else
-		key=$key"T"
-	fi
-	done
-
-	combinations[$key]=$(( ${combinations[$key]} + 1 ))
+	numOfCoins=$2
+	coin=$1
 	key=""
 
-done
+	for (( i=0;i<$numOfCoins;i++ ))
+	do
+
+		for ((j=0;j<$coin;j++))
+		do
+
+			flip=$((RANDOM%2))
+			if (( flip == 1 ))
+			then
+				key=$key"H"
+			else
+				key=$key"T"
+			fi
+		done
+
+		combinations[$key]=$(( ${combinations[$key]} + 1 ))
+		key=""
+	done
 
 	percentage ${!combinations[@]}
 }
 
 function percentage(){
 
-result=${@}
-echo
+	fatchDictValue=${@}
 
-for i in $result
-do
+	for result in $fatchDictValue
+	do
 
-	j=${combinations[$i]}
-	echo "$i	 $(( ($j * 100 ) / $number ))"
+		value=${combinations[$result]}
+		echo "$result	 $(( ($value * 100 ) / $numOfCoins ))"
 
-done | sort -k2 -nr | awk 'NR==1{print $1" "$2}'
+	done | sort -k2 -nr | awk 'NR==1{print $1" "$2}'
 }
 
-function main()
-{
+function main(){
 
-read -p "Enter your choice: 1.SingletCoin 2.DoubletCoin 3.TripletCoin " coin
-read -p "Enter How Many Times You Want to Flip " number
+	read -p "Enter your choice: 1.SingletCoin 2.DoubletCoin 3.TripletCoin " coin
+	read -p "Enter How Many Times You Want to Flip " numOfCoins
 
 	case $coin in
-	1)
-		flipCoin 1 $number;;
-	2)
-		flipCoin 2 $number;;
-	3)
-		flipCoin 3 $number;;
-	*)
-		echo "Invalid Option";;
+		1)
+			getFlipCoinResult 1 $numOfCoins;;
+		2)
+			getFlipCoinResult 2 $numOfCoins;;
+		3)
+			getFlipCoinResult 3 $numOfCoins;;
+		*)
+			echo "Invalid Option";;
 	esac
 }
 
